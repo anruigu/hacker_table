5fd028d8551dfb2833c6fa1a2d1887c73cb75f9b
Anrui Gu <anruigu@berkeley.edu>
2023-03-24 19:36:32 -0700

    scrape intro channel, private channel pending

---
+++ gpt3.py
@@ -0,0 +1,72 @@
+import openai
+import os
+import json
+import pickle
+
+with open('secrets.json') as f:
+    secrets = json.load(f)
+  
+os.environ['OPENAI_API_KEY'] = secrets['openai']
+cache_path = (
+    "cache.pkl"  # embeddings will be saved/loaded here
+)
+PROMPT_TEMPLATE = '''Generate JSON data with data extracted from this doc
+
+doc = """{text}"""
+
+This person's name is {name}.
+Extract this into a JSON dict in this format
+{{
+"about": Where does this person work and what is this person's title (string),
+"building": 1 or 2 sentences of what ideas this person is building or hacking on at the hackathon this weekend (string),
+}}
+
+data = {{
+    "about":'''
+
+# except FileNotFoundError:
+#     precomputed_cache_path = (
+#         "https://cdn.openai.com/API/examples/data/snli_embedding_cache.pkl"
+#     )
+
+    #embedding_cache = pd.read_pickle(precomputed_embedding_cache_path)
+
+def info_extract(text, name):
+    # print(os.getcwd())
+    if not os.path.isfile("cache.pkl"):
+        with open("cache.pkl",'wb') as file:
+            a = {'hi':'hi'}
+            pickle.dump(a, file)
+    with open(cache_path, "rb") as f:
+        cache = pickle.load(f)      
+
+    prompt = PROMPT_TEMPLATE.format(text=text, name=name)
+    if text not in cache.keys():
+        print("Not in cache, getting embedding...")
+        cache[text] = text_generation(prompt)
+        with open(cache_path, "wb") as cache_file:
+            pickle.dump(cache, cache_file)
+    #answers = text_generation(prompt)
+    answers = cache[text]
+    answers = answers.split('}')[0]
+    answers = '{"about":' + answers.strip() + '}'
+    # print(answers)
+    # try:
+    data = json.loads(answers)
+    assert all([i in data for i in ('about', 'building')])
+    return data
+    # except:
+    #     return {}
+
+def text_generation(prompt):
+    # send GPT-3 a prompt and get the response
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    response = openai.Completion.create(
+      model="text-davinci-003",
+      prompt=prompt,
+      temperature=0.7,
+      max_tokens=1000,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0)
+    return response['choices'][0]['text']
---
+++ test_slack.py
@@ -0,0 +1,81 @@
+import openai
+import json
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from gpt3 import info_extract
+import pandas as pd
+
+from collections import defaultdict
+
+with open('secrets.json') as f:
+    secrets = json.load(f)
+  
+openai.api_key = secrets['openai']
+slack_token = secrets['slack']
+
+
+client = WebClient(token=slack_token)
+# get all messages in the "gpt4-hackathon" channel
+channel_name = "intros"
+conversation_id = None
+try:
+    # Call the conversations.list method using the WebClient
+    for result in client.conversations_list():
+        if conversation_id is not None:
+            break 
+        for channel in result["channels"]:
+            print(channel["name"])
+            if channel["name"] == channel_name:
+                conversation_id = channel["id"]
+                # print(conversation_id)
+                #Print result
+                print(f"Found conversation ID: {conversation_id}") # save this ID
+                break
+except SlackApiError as e:
+    print(f"Error: {e}")
+
+# Store conversation history
+conversation_history = []
+# ID of the channel you want to send the message to
+channel_id = "C04EH9VQRFG"
+
+try:
+    # Call the conversations.history method using the WebClient
+    # conversations.history returns the first 100 messages by default
+    # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
+    result = client.conversations_history(channel=channel_id)
+
+    conversation_history = result["messages"]
+
+    # Print results
+    print("{} messages found in {}".format(len(conversation_history), channel_id))
+
+except SlackApiError as e:
+    print("Error creating conversation: {}".format(e))
+
+filename = 'test'
+N_ROWS = len(conversation_history)
+res = pd.DataFrame(columns=['name', 'about','building'], index=range(N_ROWS))
+NUM_FIELDS = len(res.columns) - 1
+counter = 0
+# make data table
+for convo in conversation_history:
+    # res.iloc[idx, 1:] = re.split(r'[0-9]+\*!\*: ', raw_ans)[1:]
+    user_id = convo['user']
+    try:
+        # Call the users.info method using the WebClient
+        result = client.users_info(
+            user=user_id
+        )
+        name = result['user']['profile']['real_name_normalized']
+        res.iloc[counter, 0] = name
+    except SlackApiError as e:
+        print("Error fetching conversations: {}".format(e))
+    convo_s = json.dumps(convo)
+    raw_ans = info_extract(convo_s, name) # .replace('\n','')
+    # print(raw_ans)
+    res.iloc[counter, 1] = raw_ans['about']
+    res.iloc[counter, 2] = raw_ans['building']
+    counter+=1
+
+res.to_csv("{}.csv".format(filename))
